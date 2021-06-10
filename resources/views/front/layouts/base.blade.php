e2cbbe427609dc7f33ab1d1f8f5688de1bdf9f91
naoto-naka <luckywhitecat770@gmail.com>
2021-06-10 22:20:02 +0900

    :tada: add layoutFile

--- resources/views/front/layouts/base.blade.php
+++ resources/views/front/layouts/base.blade.php
@@ -14,7 +14,7 @@
         Laravel CMS
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
-        <span class="navbar-toggle-icon"></span>
+        <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
