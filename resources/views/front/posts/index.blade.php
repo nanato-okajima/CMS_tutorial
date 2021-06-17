ff71eccb0fb002f172aed3457b79a1c11b0eb2df
naoto-naka <luckywhitecat770@gmail.com>
2021-06-17 22:48:55 +0900

    finished user_role

--- resources/views/front/posts/index.blade.php
+++ resources/views/front/posts/index.blade.php
@@ -29,14 +29,14 @@ $title = '投稿一覧';
     <li class="nav-item">
       {{ link_to_route('front.posts.index', 'すべて', null, [
         'class' => 'nav-link'.
-        (request()->segment(3) === null ? 'active' : '')
+        (request()->segment(3) === null ? ' active' : '')
       ]) }}
     </li>
     @foreach ($tags as $tag)
       <li class="nav-item">
         {{ link_to_route('front.posts.index.tag', $tag->name, $tag->slug, [
           'class' => 'nav-link'.
-          (request()->segment(3) === $tag->slug ? 'active' : '')
+          (request()->segment(3) === $tag->slug ? ' active' : '')
         ]) }}
       </li>
     @endforeach
